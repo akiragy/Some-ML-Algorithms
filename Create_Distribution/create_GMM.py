411e36572ef94356fdf1663e4fd8c9dbc6f39a89
akiragy <34157253+akiragy@users.noreply.github.com>
2018-04-30 13:38:33 +0800

    Create .py
    添加creat_GMM.py和EM4GMM.py

---
+++ Create_Distribution/create_GMM.py
@@ -0,0 +1,106 @@
+# -- coding: UTF-8 --
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+class GMM(object):
+
+    def __init__(self, K = 3, d = 3, N = 3000, interval = 3):
+        """
+        :param K: 高斯成分的数量
+        :param d: 高斯分布的维度
+        :param N: 要生成的总样本数量
+        :param interval: 各个高斯分布均值之间的间隔
+        """
+        self.K, self.d, self.N = K, d, N
+        self.interval = interval
+
+        self.X = np.zeros([N, d])  #样本集
+        self.k_true = []  #样本所属成分
+
+        self.mean_true = []  #每个成分的均值
+        self.cov_true = []  #每个成分的协方差矩阵
+
+
+    def create_cov(self):
+        """生成协方差矩阵"""
+        lmd = 0.8 * np.random.rand(self.d) + 0.1  # 精度矩阵的特征值
+        M = np.mat(np.random.randn(self.d, self.d))
+        U = np.linalg.qr(M)[0]  # QR分解产生正交矩阵
+        A = U * np.diag(lmd) * U.T  # 精度矩阵
+        return A.I  #协方差矩阵
+
+
+    def select_class(self):
+        """根据混合系数选择一个高斯成分"""
+        c_ran = np.random.rand(1)
+        sum_k = 0
+        for i, k in enumerate(self.mix_coef):
+            sum_k += k
+            if c_ran <= sum_k:
+                return i
+        return -1
+
+
+    def sample_from_gauss(self, k_sltd):
+        """从单个高斯成分中采样"""
+        if k_sltd == -1:
+            print("emm")
+            exit()
+        mean_sltd, cov_sltd = self.mean_true[k_sltd], self.cov_true[k_sltd]  #选择均值和协方差矩阵
+        x = np.random.multivariate_normal(mean_sltd, cov_sltd, 1)
+        return x
+
+
+    def create_dataset(self):
+        """生成符合GMM的数据集"""
+        for c_iter in range(self.K):
+            cur_mean = self.interval * ( np.random.rand(self.d) + 3 * c_iter)  #当前高斯成分的均值
+
+            cur_cov = self.create_cov()  #当前高斯成分的协方差矩阵
+            self.mean_true.append(cur_mean)
+            self.cov_true.append(cur_cov)
+
+        self.mix_coef = np.random.rand(self.K) + 0.3  #为了防止有的混合系数过小，故而加个数软化一下
+        self.mix_coef = self.mix_coef / sum(self.mix_coef)  #混合系数
+
+        for n_iter in range(self.N):
+            k_sltd = self.select_class()  #根据混合系数选择一个高斯成分
+            self.k_true.append(k_sltd)
+            self.X[n_iter,:] = self.sample_from_gauss(k_sltd)  #从所选高斯成分中采样
+
+
+    def plt_hist(self):
+        """样本直方图"""
+        plt.hist(self.X, normed=True, bins=100)
+        plt.title("hist of GMM dataset")
+        plt.show()
+
+
+    def plt_3d(self, d0=0, d1=1, d2=2):
+        """样本3维散点图"""
+        if self.d < 3:
+            print("样本维度不足3，无法画出3D散点图")
+            return
+        if max(d0,d1,d2) + 1 > self.d:
+            print("维度越界")
+            return
+
+        ax = plt.subplot(111, projection='3d')  #需要导入Axes3D
+        for k_iter in range(self.K):
+            k_n = [i for i in range(self.N) if self.k_true[i] == k_iter]
+            ax.scatter(self.X[k_n,d0], self.X[k_n,d1], self.X[k_n,d2])
+        ax.set_xlabel('x-axis')
+        ax.set_ylabel('y-axis')
+        ax.set_zlabel('z-axis')
+        ax.legend(('group1', 'group2', 'group3'))
+        ax.set_title('scatter diag of GMM dataset', loc='center', fontsize='large')
+        plt.show()
+
+
+if __name__ == "__main__":
+    gmm = GMM(d = 4)
+    gmm.create_dataset()
+    gmm.plt_hist()
+    gmm.plt_3d()
+    gmm.plt_3d(d0=4)
---
+++ EM_alg/EM4GMM.py
@@ -0,0 +1,154 @@
+# -- coding: UTF-8 --
+import sys
+sys.path.append("..")
+from Create_Distribution import create_GMM
+
+import numpy as np
+from numpy import mat
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+class EM_for_GMM(object):
+
+    def __init__(self, K = 3, d = 3, N = 1000, max_iter = 200):
+        np.random.seed(2018)
+
+        self.gmm = create_GMM.GMM(K, d, N, interval=1)
+        self.gmm.create_dataset()
+
+        self.K, self.d, self.N = K, d, N  #K成分数量，d变量维数，N样本个数
+        self.max_iter = max_iter  #EM算法最大迭代次数
+        self.iter = self.max_iter  #实际迭代次数
+
+        self.X =self.gmm.X  #样本集
+        self.k_true = self.gmm.k_true  #样本所属成分
+
+        self.mean_true = self.gmm.mean_true  #每个成分的均值
+        self.cov_true = self.gmm.cov_true  #每个成分的协方差矩阵
+
+
+    def pdf_gauss(self, d, mean, cov, x):
+        c = (2*np.pi)**(-0.5*d) * np.linalg.det(cov)**(-0.5)
+        quad = -0.5 * mat(x-mean) * mat(cov).I * mat(x-mean).T
+        p = c * np.exp(quad)
+        return p
+
+
+    def E_step(self):
+        self.gamma = np.zeros([self.N, self.K])  #后验概率
+        for n_iter in range(self.N):
+            z = np.zeros(self.K)
+            for k in range(self.K):
+                z[k] = self.mix_coef_em[k] * self.pdf_gauss(self.d,self.mean_em[k],self.cov_em[k],self.X[n_iter,:])
+            z = z / sum(z)
+            self.gamma[n_iter,:] = z
+
+
+    def M_step(self):
+        for k_iter in range(self.K):
+            mm, mc = 0, 0
+            for n_iter in range(self.N):
+                mm += self.gamma[n_iter,k_iter] * self.X[n_iter,:]
+            self.mean_em[k_iter] = mm / sum(self.gamma[:,k_iter])  #更新均值
+            for n_iter in range(self.N):
+                mc += self.gamma[n_iter,k_iter] * mat(self.X[n_iter,:]-self.mean_em[k_iter]).T * mat(self.X[n_iter,:]-self.mean_em[k_iter])
+            self.cov_em[k_iter] = mc / sum(self.gamma[:,k_iter])  #更新协方差矩阵
+            self.mix_coef_em[k_iter] = sum(self.gamma[:,k_iter]) / self.N  #更新混合系数
+
+
+    def calc_ll(self):
+        '''计算似然函数'''
+        ll = 0
+        for n in range(self.N):
+            sll = 0
+            for k in range(self.K):
+                sll += self.mix_coef_em[k] * self.pdf_gauss(self.d,self.mean_em[k],self.cov_em[k],self.X[n,:])
+            ll += np.log(sll)
+        return ll
+
+
+    def sep_class(self):
+        '''根据EM算法结果划分聚簇'''
+        self.k_em = []
+        for n_iter in range(self.N):
+            gamma_n = self.gamma[n_iter,:]
+            k_n = [i for i in range(self.K) if gamma_n[i] == max(gamma_n)]
+            self.k_em.append(k_n[0])
+
+
+    def EM_alg(self):
+
+        self.mean_em = [self.X[i,:] for i in range(self.K)]  #初始化均值
+        self.cov_em = [np.eye(self.d) for i in range(self.K)]  #初始化协方差矩阵
+        self.mix_coef_em = [1.0/self.K for i in range(self.K)]  #初始化混合系数
+
+        ll_cur = self.calc_ll()
+        for n_iter in range(self.max_iter):
+            ll_pre = ll_cur
+            self.E_step()
+            self.M_step()
+            ll_cur = self.calc_ll()
+
+            if n_iter % 10 == 0:
+                print("第" + str(n_iter+1) + "次迭代后的似然函数为" + str(ll_cur))
+
+            if ll_cur - ll_pre < 1e-4:
+                self.iter = n_iter + 1
+                break
+        self.sep_class()
+
+
+    def plt_2d(self, ax_0=0, ax_1=1):
+        '''作任意两个维度之间的散点图'''
+        plt.subplot(121)  #groundtruth
+        for k_iter in range(self.K):
+            k_n = [i for i in range(self.N) if self.k_true[i] == k_iter]
+            plt.scatter(self.X[k_n,ax_0], self.X[k_n,ax_1])
+
+        plt.subplot(122)  #算法结果
+        for k_iter in range(self.K):
+            k_n = [i for i in range(self.N) if self.k_em[i] == k_iter]
+            plt.scatter(self.X[k_n,ax_0], self.X[k_n,ax_1])
+        plt.show()
+
+
+    def plt_3d(self, d0=0, d1=1, d2=2):
+        '''作3维散点图'''
+        if self.d < 3:
+            print("样本维度不足3，无法画出3D散点图")
+            return
+        if max(d0,d1,d2) + 1 > self.d:
+            print("维度越界")
+            return
+
+        ax = plt.subplot(121, projection='3d')  #groundtruth。需要导入Axes3D
+        for k_iter in range(self.K):
+            k_n = [i for i in range(self.N) if self.k_true[i] == k_iter]
+            ax.scatter(self.X[k_n,d0], self.X[k_n,d1], self.X[k_n,d2])
+        ax.set_xlabel('x-axis')
+        ax.set_ylabel('y-axis')
+        ax.set_zlabel('z-axis')
+        ax.legend(('group1', 'group2', 'group3'))
+        ax.set_title('groundtruth', loc='center', fontsize='large')
+
+        ax = plt.subplot(122, projection='3d')  #算法结果
+        for k_iter in range(self.K):
+            k_n = [i for i in range(self.N) if self.k_em[i] == k_iter]
+            ax.scatter(self.X[k_n,d0], self.X[k_n,d1], self.X[k_n,d2])
+        ax.set_xlabel('x-axis')
+        ax.set_ylabel('y-axis')
+        ax.set_zlabel('z-axis')
+        ax.legend(('group1', 'group2', 'group3'))
+        ax.set_title('result of EM', loc='center', fontsize='large')
+
+        plt.show()
+
+
+if __name__ == "__main__":
+
+    ee = EM_for_GMM()
+    ee.EM_alg()
+    ee.plt_2d(0,1)
+    ee.plt_3d()
+    ee.plt_3d(d0=3)
+
